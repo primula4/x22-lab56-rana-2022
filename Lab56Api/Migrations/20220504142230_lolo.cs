1c661a3e005ad29e5ee5903d18815d6b0152ffda
primula4 <6180817@cegep-heritage.qc.ca>
2022-05-04 11:10:19 -0400

    add lab56api

---
+++ Lab56Api/Migrations/20220504142230_lolo.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Lab56Api.Migrations
+{
+    public partial class lolo : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
